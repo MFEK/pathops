ea87ee0e0b7297113db025fb038cb8aaaa5f1716
T Prajwal Prabhu <prajwalprabhu.tellar@gmail.com>
2021-12-25 18:46:25 +0530

    create_bezzier

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@
 
 use clap::{App, AppSettings};
 use env_logger;
-#[allow(unused)]
-mod validators;
+// #[allow(unused)]
+// mod validators;
 
 mod boolean;
-
+mod create_bezier;
 fn main() {
     env_logger::init();
     #[allow(unused_mut)] // we actually use it if cfg(feature=fontforge)
@@ -17,12 +17,14 @@ fn main() {
         .setting(AppSettings::DisableHelpSubcommand)
         .author("Fredrick R. Brennan <copypasteⒶkittens⊙ph>; MFEK Authors")
         .about("A utility for applying path operations to contours (in UFO .glif format).")
-        .subcommand(boolean::clap_app());
+        .subcommand(boolean::clap_app())
+        .subcommand(create_bezier::clap_app());
 
     let matches = argparser.get_matches();
 
     match matches.subcommand_name() {
         Some("BOOLEAN") => boolean::cli(matches.subcommand_matches("BOOLEAN").unwrap()),
+        Some("CREATE") => create_bezier::cli(matches.subcommand_matches("CREATE").unwrap()),
         _ => {
             unreachable!()
         }
